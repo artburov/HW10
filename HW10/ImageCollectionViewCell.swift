f1fcb8688aac4b6d5bee39b14e268cd91d2cafc1
Khan <burov@ukr.net>
2020-05-07 01:39:59 +0300

    HW10

---
+++ HW10/ImageCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCollectionViewCell.swift
+//  HW10
+//
+//  Created by Admin on 06.05.2020.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+}
--- HW10/ViewController.swift
+++ HW10/ViewController.swift
@@ -14,7 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
+}
 
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let w = UIScreen.main.bounds.size.width / 2
+        return CGSize(width: w, height: w)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath) as! ImageCollectionViewCell
+        cell.titleLabel.text = "Text"
+        cell.priceLabel.text = "139$"
+        cell.imageCell.image = #imageLiteral(resourceName: "11")
+        return cell
+    }
+    
+    
 }
 
