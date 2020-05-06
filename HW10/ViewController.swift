6dcc57805634560a49b8ff208d8f2b23e420a036
Khan <burov@ukr.net>
2020-05-06 22:31:59 +0300

    Initial Commit

---
+++ HW10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW10
+//
+//  Created by Admin on 06.05.2020.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
